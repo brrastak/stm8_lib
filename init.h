25a11fef0ba60b0975bd38d2a795e00a23919120
brrastak <54371570+brrastak@users.noreply.github.com>
2020-12-09 15:10:05 +0300

    Add files via upload

---
+++ init.h
@@ -0,0 +1,21 @@
+
+#ifndef __INIT_H__
+#define __INIT_H__
+
+
+// Init interrupts
+void InitInterrupt(void);
+void EnableInterrupt(void);
+void DisableInterrupt(void);
+
+// Init GPIO
+void InitGpio(void);
+
+// Init clock
+void InitRcc(void);
+
+// Deinit unused modules
+void DeinitPeriph(void);
+
+
+#endif
